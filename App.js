2018bbab6737bd29be8704ed0d19a75d29312665
tyseschl <sebastian.schleiken@stud.hs-ruhrwest.de>
2022-01-02 18:05:51 +0100

    Create startpage

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Datenbank anlegen"/>
+      <Button title="Datenbanken verwalten"/>
+      <Button title="Datenbanken löschen"/>
+      <Button title="Einstellungen"/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#81DAF5',
     alignItems: 'center',
     justifyContent: 'center',
   },

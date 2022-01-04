c2629885d2d003678dd59d2774673ff85ab2c297
tyseschl <sebastian.schleiken@stud.hs-ruhrwest.de>
2022-01-04 17:15:15 +0100

    Neues Menue

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import { Settings } from './Screens/Settings.js';
 import { EditDatabase } from './Screens/EditDatabase.js';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import * as Icon from '@expo/vector-icons';
+import { ColorPropType } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
@@ -14,9 +15,38 @@ export default function App() {
   return (
   <NavigationContainer >
     <Tab.Navigator>
-      <Tab.Screen name="CreateDB" component={CreateDatabase}/>
-      <Tab.Screen name="EditDB" component={EditDatabase}/>
-      <Tab.Screen name="Settings" component={Settings}/>
+      <Tab.Screen
+      name="CreateDB" 
+      component={CreateDatabase}
+      options={{title:'Erstellen',
+      tabBarIcon: ({focused, size, color}) => 
+        <Icon.Ionicons 
+          name="create" 
+          size={size}
+          color={color}
+        />
+      }}
+      />
+      <Tab.Screen name="EditDB" component={EditDatabase}
+      options={{title:'Bearbeiten',
+      tabBarIcon: ({focused, size, color}) => 
+        <Icon.MaterialCommunityIcons 
+          name="qrcode-edit" 
+          size={size}
+          color={color}
+        />
+      }}
+      />
+      <Tab.Screen name="Settings" component={Settings}
+      options={{title:'Einstellungen',
+      tabBarIcon: ({focused, size, color}) => 
+        <Icon.Ionicons 
+          name="ios-settings-sharp" 
+          size={size}
+          color={color}
+        />
+      }}
+      />
     </Tab.Navigator>
   </NavigationContainer>);
 }
---
+++ AppNavigator.js
@@ -0,0 +1,23 @@
+import { CreateDatabase } from './Screens/CreateDatabase';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+
+const Tab = createBottomTabNavigator();
+
+
+
+export  function AppNavigator() {
+  
+  return (
+  <NavigationContainer >
+    <Tab.Navigator>
+      <Tab.Screen name="CreateDB" component={CreateDatabase}
+        options={{tabBarIcon: ({focused, size, color})}}
+      />
+      <Tab.Screen name="EditDB" component={EditDatabase}/>
+      <Tab.Screen name="Settings" component={Settings}/>
+    </Tab.Navigator>
+  </NavigationContainer>);
+}
+
